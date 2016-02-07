874a5b1c77a72e98a6014e243cdec304b2f1e882
Galimov Ruslan <delfin1995@yandex.ru>
2016-02-07 03:35:15 +0300

    Init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-
+#include <fstream>
+#include <algorithm>
+#include <iostream>
 #include "map.h"
 
+void read_coordinates(){ // char*, Point**
+    std::ifstream infile("C:\\Users\\Ruslan\\ClionProjects\\project\\coordinates.csv");
+    std::string line;
+    while (std::getline(infile, line)) {
+        std::replace(line.begin(), line.end(), ';', ' ');
+        std::cout << line << std::endl;
+    }
+}
+
 int main() {
-    Map map();
+//Map map();
+    read_coordinates();
 }
--- point.h
+++ point.h
@@ -5,7 +5,7 @@
 #ifndef PROJECT_POINT_H
 #define PROJECT_POINT_H
 #define pointType unsigned char
-#define cordType int
+#define cordType int  // 0.1 мм
 #define levelType unsigned char
 
 struct Point{
